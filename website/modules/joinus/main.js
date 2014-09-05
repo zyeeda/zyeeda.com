d98022a7adc3a5ac03c9fda814b122c3be08bb30
chenhaifeng <7720829@gmail.com>
2014-09-05 14:48:16 +0800

    调整hr邮箱图标及气球大小

--- website/modules/joinus/main.js
+++ website/modules/joinus/main.js
@@ -4,14 +4,14 @@ $(function() {
 		$("#nav #joinus-li").addClass('active');
 	});
 
-	$('.position-op .msg').on('mouseover', function() {
-		$(this).hide();
-		$(this).next().fadeIn(1000);
-	})
+	// $('.position-op .msg').on('mouseover', function() {
+	// 	$(this).hide();
+	// 	$(this).next().fadeIn(1000);
+	// })
 
-	$('.position-op .send').on('mouseout', function() {
-		$(this).hide();
-		$(this).prev().fadeIn(1000);
-	})
+	// $('.position-op .send').on('mouseout', function() {
+	// 	$(this).hide();
+	// 	$(this).prev().fadeIn(1000);
+	// })
 
 })

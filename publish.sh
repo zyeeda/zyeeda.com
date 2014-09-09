8b6a4bcb51d8c6b42cd740d05e161cc8eeb60dcc
Rui Tang <tangrui.cn@gmail.com>
2014-09-09 15:18:15 +0800

    添加发布到服务器的自动化脚本.

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+rm -rf public/
+
+cd website
+fis release -opmd default
+
+cd ..
+scp -r public/ zyeeda@173.230.132.22:
+
+ssh -t zyeeda@173.230.132.22 'sudo cp -vR public/ /usr/share/nginx/www.zyeeda.com; rm -rf public'

7071352b765f7a2001f3a46f9a395fbc4c15bee4
Rui Tang <tangrui.cn@gmail.com>
2014-09-14 13:15:04 +0800

    Add Baidu Tongji tracking code.

--- website/modules/common/main.js
+++ website/modules/common/main.js
@@ -6,6 +6,7 @@ var scrollEvent = function(me, evt) {
     evt.preventDefault();
 };
 
+// Google Analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -16,9 +17,11 @@ ga('require', 'displayfeatures');
 ga('require', 'linkid', 'linkid.js');
 ga('send', 'pageview');
 
+// Baidu Tongji
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F3ec6f9f451decaf0b7e663ff7cc45b45' type='text/javascript'%3E%3C/script%3E"));
 
 $(function() {
-
     var wow = new WOW({
       boxClass: 'wow',
       animateClass: 'animated',

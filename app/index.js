1071584d125174887d8ab4c703cd1eb10844ae7f
vimniky <vimniky@gmail.com>
2016-04-26 15:54:06 +0800

    remove random query string appended in url

--- app/index.js
+++ app/index.js
@@ -1,17 +1,20 @@
 import React from "react";
 import {render} from "react-dom";
-import {Router, hashHistory ,browserHistory ,Route , IndexRoute} from "react-router";
+import {Router,useRouterHistory ,Route , IndexRoute} from "react-router";
 import App from "./components/App";
 import About from "./components/About";
 import Join from "./components/Join";
 import Home from "./components/Home";
 
+import { createHashHistory } from 'history';
+// useRouterHistory creates a composable higher-order function
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
 import './styles/main.scss';
 import './styles/libs/pure-grids.min.css';
 
 render((
-    <Router history={hashHistory}>
+    <Router history={appHistory}>
         <Route path="/" component={App}>
             <IndexRoute component={Home}/>
             <Route path="/joinus" component={Join}/>

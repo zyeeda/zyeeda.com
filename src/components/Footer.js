ffb82a9839fdc76ef28ea5e5fed1f0dd62a73e73
shupeipei <shupeipei@zyeeda.com>
2016-06-28 09:45:22 +0800

    correct corp name in footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,23 +10,23 @@ export default React.createClass({
         return (
             <div className='footer' id='footer'>
                 <div className="infos">
-                    <div >
+                    <div>
                         <img src="../images/home/map.png"/>
                     </div>
-                    <div >
+                    <div>
                         <img className="logo" src="../images/logos/logo-white.svg"/>
                     </div>
-                    <div >
+                    <div>
                         <p></p>
                         <div>
-                            <p>深圳中昱达信息技术有限责任公司</p>
+                            <p>深圳市中昱达信息技术有限责任公司</p>
                             <p >电话: 4008226278</p>
                         </div>
                         <p></p>
                     </div>
-                    <div >
+                    <div>
                         <p>地址: 深圳市罗湖区怡景路2008号国家动漫画产业基地A区305室 (邮编518021) </p>
-                        <p >传真: 0755-26966586-802</p>
+                        <p>传真: 0755-26966586-802</p>
                     </div>
                 </div>
                <p className="copy-right">
